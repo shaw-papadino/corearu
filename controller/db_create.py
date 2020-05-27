16d4e7a35553fe2639537cbdebf2d245fadb4ec6
shoyaokayama <sho.edu.human@gmail.com>
2020-05-28 00:38:08 +0900

    add postgres

--- controller/db_create.py
+++ controller/db_create.py
@@ -9,10 +9,11 @@ from sqlalchemy.ext.declarative import declarative_base
 # SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
 # 本番はpostgre
 # SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
-SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL") or "sqlite:///./sql_app.db"
 
-engine = create_engine(
-        SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False}
-        )
+# engine = create_engine(
+#         SQLALCHEMY_DATABASE_URL, connect_args = {"check_same_thread": False}
+#         )
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = scoped_session(sessionmaker(autocommit = False, autoflush = False, bind = engine))
 Base = declarative_base()
